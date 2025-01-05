9478faaea8aa178080f41c46f55dc0db660787d0
Shreshth053 <mishra.shreshtha2003@gmail.com>
2025-01-05 23:37:25 +0530

    modified in substr.cpp file

--- substr.cpp
+++ substr.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     string str="shreshtha";
     int middle=str.length()/2;
-    cout<<"first half: "<<str.substr(0,middle)<<" "<<"second half: "<<str.substr(middle);
+    cout<<"first half is: "<<str.substr(0,middle)<<" "<<"second half is: "<<str.substr(middle);
 
     return 0;
 }
